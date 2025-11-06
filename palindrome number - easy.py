83b36b8792481a5bb00309c3b7d104908c79391a
CMP_BaThompson <CMP_BaThompson>
2025-11-06 09:36:40 -0500

    initial commit

--- palindrome number - easy.py
+++ palindrome number - easy.py
@@ -6,8 +6,8 @@ if number == number[::-1]:
     print(f"x = {number}")
     print("True, the number is a palindrome")
     print(f" - Explanation: {number} reads as {number} from left to right and from right to left.\n")
+    
 if number != number[::-1]:
     print(f"x = {number}")
     print("False, the number is not a palindrome")
     print(f" - Explanation: From left to right, it reads {number}. From right to left, it is not the same number. Therefore it is not a palindrome.\n")
-
--- two sum - easy.py
+++ two sum - easy.py
@@ -1,1 +1,13 @@
-number = input("Input a")
+def two_sum(numlist, target):
+    #loop through the numlist:
+    for idx, num in enumerate(numlist):
+        print(f"{idx}: {num} looking for {target-num}")
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(two_sum([2,7,11,15], 9))
+    print(two_sum([3,2,4], 6))
